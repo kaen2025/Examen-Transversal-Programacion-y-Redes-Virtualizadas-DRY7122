53674535f5804d5e55ac47066ee668945f6136d4
kaen2025 <carl.arriaza@duocuc.cl>
2025-07-02 02:10:09 +0000

    actualice todo

--- vlan.py
+++ vlan.py
@@ -2,8 +2,8 @@ vlan = input("Ingrese el número de VLAN: ") # pide el numero de vlan easy
 vlan_num = int(vlan) # esto es porque la vlan siempre sera texto string y se requiere que sea numero entero.
 
 if vlan_num >= 1 and vlan_num <= 1005:
-    print("La VLAN es del rango NORMAL.")
+    print("La VLAN es del rango normal.")
 elif vlan_num >= 1006 and vlan_num <= 4094:
-    print("La VLAN es del rango EXTENDIDO.")
+    print("La VLAN es del rango extendido.")
 else:
     print("Número de VLAN fuera del rango válido.")
