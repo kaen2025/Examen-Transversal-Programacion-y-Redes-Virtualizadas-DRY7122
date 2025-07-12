5aa0bb82855a61c338cb08055db5e718ba7375fc
kaen2025 <carl.arriaza@duocuc.cl>
2025-07-12 15:29:36 +0000

    item 1 integrantes

--- item1.py
+++ item1.py
@@ -5,4 +5,4 @@ integrantes = [  # Crea una lista llamada integrantes que guarda los nombres de 
 
 print("Integrantes del grupo:") #imprime el titulo lol.
 for nombre in integrantes: #esta vaina es un bucle "for" para imprimir la lista integrantes.
-    print("-", nombre) #el guion hace que por cada nombre imprima un guion, lo puse porque se ve lindo
+    print("-", nombre) #el guion hace que por cada nombre imprima un guión
