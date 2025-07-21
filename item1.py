67c842755c993b2268b7ade76f0c047194f0b8e5
kaen2025 <carl.arriaza@duocuc.cl>
2025-07-20 20:33:54 -0400

    item 1

--- item1.py
+++ item1.py
@@ -1,8 +1,8 @@
-integrantes = [  # Crea una lista llamada integrantes que guarda los nombres de las personas (son textos).
+integrantes = [  
     "Carlos Arriaza",
     "Fernanda Avila"
 ]
 
-print("Integrantes del grupo:") #imprime el titulo lol.
-for nombre in integrantes: #esta vaina es un bucle "for" para imprimir la lista integrantes.
-    print("-", nombre) #el guion hace que por cada nombre imprima un guión
+print("Integrantes del grupo:")
+for nombre in integrantes: 
+    print("-", nombre) 
